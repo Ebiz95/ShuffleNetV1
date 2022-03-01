21b89ddf855c636c37165e7ff89ea3f46a846a98
Ebiz95 <ebaa.asaad95@gmail.com>
2022-03-01 18:18:55 +0100

    Cleaner code

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers, regularizers, activations
+from tensorflow.keras import layers
 
 
 class FirstConv1x1Block(layers.Layer):
