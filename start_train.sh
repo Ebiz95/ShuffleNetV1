6fac564d07ebb2097ab2037f3ac75a3e467e225a
Ebiz95 <ebaa.asaad95@gmail.com>
2022-03-08 11:18:28 +0100

    Edit csv dir

--- start_train.sh
+++ start_train.sh
@@ -1,3 +1,3 @@
 mkdir data
-python3 divide_boat_no_boat.py --sourcedir ../data/train/images --csv ../data/train_ship_segmentation_v2.csv --destdir ./data
+python3 divide_boat_no_boat.py --sourcedir ../data/train/images --csv ../data/train_ship_segmentations_v2.csv --destdir ./data
 python3 trian --batch-size 128 --epochs 100 --save ../project/shuffleNet-results/models 
